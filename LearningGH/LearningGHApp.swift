39484132b6d604aaa21772f397c1f60a989068ca
KiwiBlueSpring <72439854+KiwiBlueSpring@users.noreply.github.com>
2022-07-11 13:42:27 +1200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Calum de Fanti on 11/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
